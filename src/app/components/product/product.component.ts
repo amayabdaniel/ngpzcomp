6aa0b0d349d3a0cd9a88166d95e8a27ad694a510
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2023-01-17 21:38:41 -0500

    avance 2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Product } from '../../models/product.model'
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+
+  product: Product = {
+      id: '1',
+      name: 'Product 1',
+      image: 'assets/images/pexels-pixabay-266621.jpg',
+      price: 100
+  }
 
 }
